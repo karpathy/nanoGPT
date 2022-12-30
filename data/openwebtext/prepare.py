324881efc92c0aba826d12b9744ca8e62809856a
Thomas Vogt <mehrmorgen@me.com>
2022-12-30 22:18:20 +0000

    simplify the prepare script a lot, write only using one process, seems sufficient for now. ty @LaihoE for suggestion and @proger for flagging
    (cherry picked from commit 63ebf1549fdb4fdb648bb6ff4146fbb3f2711e9d)

--- data/openwebtext/prepare.py
+++ data/openwebtext/prepare.py
@@ -1,7 +1,6 @@
 # saves the openwebtext dataset to a binary file for training. following was helpful:
 # https://github.com/HazyResearch/flash-attention/blob/main/training/src/datamodules/language_modeling_hf.py
 
-import os
 from tqdm import tqdm
 import numpy as np
 import tiktoken
@@ -22,38 +21,19 @@ if __name__ == '__main__':
     # takes 54GB in huggingface .cache dir, about 8M documents (8,013,769)
     dataset = load_dataset("openwebtext", num_proc=num_proc_load_dataset)
 
-    # owt by default only contains the 'train' split, so create a test split
-    split_dataset = dataset["train"].train_test_split(test_size=0.0005, seed=2357, shuffle=True)
-    split_dataset['val'] = split_dataset.pop('test') # rename the test split to val
+# concatenate all the ids in each dataset into one large file we can use for training
+for split, dset in tokenized.items():
+    arr_len = np.sum(dset['len'])
+    filename = f'{split}.bin'
+    dtype = np.uint16 # (can do since enc.max_token_value == 50256 is < 2**16)
+    arr = np.memmap(filename, dtype=dtype, mode='w+', shape=(arr_len,))
 
-    # this results in:
-    # >>> split_dataset
-    # DatasetDict({
-    #     train: Dataset({
-    #         features: ['text'],
-    #         num_rows: 8009762
-    #     })
-    #     val: Dataset({
-    #         features: ['text'],
-    #         num_rows: 4007
-    #     })
-    # })
-
-    # we now want to tokenize the dataset. first define the encoding function (gpt2 bpe)
-    def process(example):
-        ids = enc.encode_ordinary(example['text']) # encode_ordinary ignores any special tokens
-        ids.append(enc.eot_token) # add the end of text token, e.g. 50256 for gpt2 bpe
-        # note: I think eot should be prepended not appended... hmm. it's called "eot" though...
-        out = {'ids': ids, 'len': len(ids)}
-        return out
-
-    # tokenize the dataset
-    tokenized = split_dataset.map(
-        process,
-        remove_columns=['text'],
-        desc="tokenizing the splits",
-        num_proc=num_proc,
-    )
+    print(f"writing {filename}...")
+    idx = 0
+    for example in tqdm(dset):
+        arr[idx : idx + example['len']] = example['ids']
+        idx += example['len']
+    arr.flush()
 
     # concatenate all the ids in each dataset into one large file we can use for training
     for split, dset in tokenized.items():
