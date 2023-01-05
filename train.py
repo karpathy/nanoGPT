48157ee8c1b1d426e48da53b767eb2e08a65452d
Thomas Vogt <mehrmorgen@me.com>
2023-01-05 09:09:22 +0100

    Make wandb import conditioned to wandb_log=True
    (cherry picked from commit f0864358274b9b1d29aa6dfcaef4221559672f76)

--- train.py
+++ train.py
@@ -97,10 +97,9 @@ if master_process:
 torch.manual_seed(1337 + seed_offset)
 torch.backends.cuda.matmul.allow_tf32 = True # allow tf32 on matmul
 torch.backends.cudnn.allow_tf32 = True # allow tf32 on cudnn
-device_type = 'cuda' if 'cuda' in device else 'cpu' # for later use in torch.autocast
-# note: float16 data type will automatically use a GradScaler
-ptdtype = {'float32': torch.float32, 'bfloat16': torch.bfloat16, 'float16': torch.float16}[dtype]
-ctx = nullcontext() if device_type == 'cpu' else torch.amp.autocast(device_type=device_type, dtype=ptdtype)
+# import wandb conditionally
+if wandb_log:
+    import wandb
 
 # poor man's data loader
 data_dir = os.path.join('data', dataset)
