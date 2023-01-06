31102cc11c90e9dd607b04b714dd12ee6e4193e3
Anri Lombard <anri.m.lombard@gmail.com>
2023-01-06 13:40:07 +0200

    Inefficiencies
    One inefficiency in the previous code is that the q, k, and v tensors are each split from the output of the c_attn linear layer and then reshaped to have the shape (B, T, self.n_head, C // self.n_head) using the view() method. This reshaping process can be combined with the split() operation.
    
    The y tensor is created by reshaping y.transpose(1, 2).contiguous().view(B, T, C) to the shape (B, T, C), which can be simplified by using the view_as() method.

--- model.py
+++ model.py
@@ -44,10 +44,7 @@ class CausalSelfAttention(nn.Module):
         B, T, C = x.size() # batch size, sequence length, embedding dimensionality (n_embd)
 
         # calculate query, key, values for all heads in batch and move head forward to be the batch dim
-        q, k ,v  = self.c_attn(x).split(self.n_embd, dim=2)
-        k = k.view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs)
-        q = q.view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs)
-        v = v.view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs)
+        q, k ,v  = self.c_attn(x).view(B, T, self.n_head, C // self.n_head).transpose(1, 2).split(self.n_embd, dim=3) # (B, nh, T, hs)
 
         # causal self-attention; Self-attend: (B, nh, T, hs) x (B, nh, hs, T) -> (B, nh, T, T)
         att = (q @ k.transpose(-2, -1)) * (1.0 / math.sqrt(k.size(-1)))
@@ -55,7 +52,7 @@ class CausalSelfAttention(nn.Module):
         att = F.softmax(att, dim=-1)
         att = self.attn_dropout(att)
         y = att @ v # (B, nh, T, T) x (B, nh, T, hs) -> (B, nh, T, hs)
-        y = y.transpose(1, 2).contiguous().view(B, T, C) # re-assemble all head outputs side by side
+        y = y.transpose(1, 2).view_as(x) # re-assemble all head outputs side by side
 
         # output projection
         y = self.resid_dropout(self.c_proj(y))
