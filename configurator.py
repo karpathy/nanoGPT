1fd76431b0f0ed63b6c80429bf90e82b3654b2f4
Arthur B <murbard@users.noreply.github.com>
2023-01-07 18:29:44 -0500

    Use argparse but every variable remains at the global level and there's minimal boilerplate

--- configurator.py
+++ configurator.py
@@ -1,47 +1,30 @@
-"""
-Poor Man's Configurator. Probably a terrible idea. Example usage:
-$ python train.py config/override_file.py --batch_size=32
-this will first run config/override_file.py, then override batch_size to 32
+import argparse
+import inspect
+from dataclasses import dataclass, fields
+import tokenize
+import io
 
-The code in this file will be run as follows from e.g. train.py:
->>> exec(open('configurator.py').read())
+# The previous version of this file was a "Poor Man's Configurator" and was described
+# as "probably a terrible idea". This is also probably a terrible idea, but it
+# brings some argparse sanity, while preserving the requirements outlined in the
+# previous version:
+# - avoiding configuration complexity
+# - not having to prepend config
 
-So it's not a Python module, it's just shuttling this code away from train.py
-The code in this script then overrides the globals()
-
-I know people are not going to love this, I just really dislike configuration
-complexity and having to prepend config. to every single variable. If someone
-comes up with a better simple Python solution I am all ears.
-"""
-
-import sys
-from ast import literal_eval
-
-for arg in sys.argv[1:]:
-    if '=' not in arg:
-        # assume it's the name of a config file
-        assert not arg.startswith('--')
-        config_file = arg
-        print(f"Overriding config with {config_file}:")
-        with open(config_file) as f:
-            print(f.read())
-        exec(open(config_file).read())
-    else:
-        # assume it's a --key=value argument
-        assert arg.startswith('--')
-        key, val = arg.split('=')
-        key = key[2:]
-        if key in globals():
-            try:
-                # attempt to eval it it (e.g. if bool, number, or etc)
-                attempt = literal_eval(val)
-            except (SyntaxError, ValueError):
-                # if that goes wrong, just use the string
-                attempt = val
-            # ensure the types match ok
-            assert type(attempt) == type(globals()[key])
-            # cross fingers
-            print(f"Overriding: {key} = {attempt}")
-            globals()[key] = attempt
-        else:
-            raise ValueError(f"Unknown config key: {key}")
+def arguments(cls):
+    parser = argparse.ArgumentParser( formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    source = inspect.getsource(cls)
+    source_bytes = source.encode("utf-8")
+    source_file = io.BytesIO(source_bytes)
+    tokens = tokenize.tokenize(source_file.readline)
+    tokens = {t.string:t.line for t in tokens}
+    for field in fields(cls):
+        comment = None
+        try:
+            comment = tokens[field.name].split('#')[1].strip()
+        except:
+            pass
+        parser.add_argument(f"--{field.name}", type=field.type, default=field.default, help=comment)
+    args = parser.parse_args()
+    cls.__init__ = lambda self : self.__dict__.update(args.__dict__)
+    return cls
--- train.py
+++ train.py
@@ -21,47 +21,48 @@ from torch.distributed import init_process_group, destroy_process_group
 
 from model import GPTConfig, GPT
 
-# -----------------------------------------------------------------------------
-# default config values designed to train a gpt2 (124M) on OpenWebText
-# I/O
-out_dir = 'out'
-eval_interval = 2000
-log_interval = 1
-eval_iters = 200
-eval_only = False # if True, script exits right after the first eval
-always_save_checkpoint = True # if True, always save a checkpoint after each eval
-init_from = 'scratch' # 'scratch' or 'resume' or 'gpt2*'
-# wandb logging
-wandb_log = False # disabled by default
-wandb_project = 'owt'
-wandb_run_name = 'gpt2' # 'run' + str(time.time())
-# data
-dataset = 'openwebtext'
-batch_size = 12
-block_size = 1024
-# model
-n_layer = 12
-n_head = 12
-n_embd = 768
-dropout = 0.0 # for pretraining 0 is good, for finetuning try 0.1+
-# adamw optimizer
-learning_rate = 6e-4 # max learning rate
-max_iters = 600000 # total number of training iterations
-weight_decay = 1e-2
-betas = (0.9, 0.95)
-# learning rate decay settings
-decay_lr = True # whether to decay the learning rate
-warmup_iters = 2000 # how many steps to warm up for
-lr_decay_iters = 600000 # should be ~= max_iters per Chinchilla
-min_lr = 6e-5 # minimum learning rate, should be ~= learning_rate/10 per Chinchilla
-# DDP settings
-backend = 'nccl' # 'nccl', 'gloo', etc.
-# system
-device = 'cuda'
-compile = True # use PyTorch 2.0 to compile the model to be faster
-# -----------------------------------------------------------------------------
-exec(open('configurator.py').read()) # overrides from command line or config file
-# -----------------------------------------------------------------------------
+from configurator import arguments
+from dataclasses import dataclass
+
+@arguments
+@dataclass
+class Args:
+    out_dir: str = 'out' # output directory
+    eval_interval: int = 2000 # number of iterations between evaluations
+    log_interval: int = 1 # number of iterations between logging
+    eval_iters: int = 200 # number of iterations to run evaluation for
+    eval_only: bool = False # if True, script exits right after the first eval
+    always_save_checkpoint: bool = True # if True, always save a checkpoint after each eval
+    init_from: str = 'scratch' # scratch, resume, or gpt2*
+    wandb_log: bool = False # whether to log to wandb
+    wandb_project: str = 'owt' # wandb project name
+    wandb_run_name: str = 'gpt2' # wandb run name
+    dataset: str = 'openwebtext' # dataset name
+    batch_size: int = 12 # batch size
+    block_size: int = 1024 # block size
+    n_layer: int = 12 # number of layers
+    n_head: int = 12 # number of heads
+    n_embd: int = 768 # embedding dimension
+    dropout: float = 0.0 # dropout rate
+    learning_rate: float = 6e-4 # max learning rate
+    max_iters: int = 600000 # total number of training iterations
+    weight_decay: float = 1e-2 # weight decay
+    beta_1: float = 0.9 # beta_1
+    beta_2: float = 0.95 # beta_2
+    decay_lr: bool = True # whether to decay the learning rate
+    warmup_iters: int = 2000 # how many steps to warm up for
+    lr_decay_iters: int = 600000 # should be ~= max_iters per Chinchilla
+    min_lr: float = 6e-5 # minimum learning rate, should be ~= learning_rate/10 per Chinchilla
+    backend: str = 'nccl' # nccl, gloo, etc.
+    device: str = 'cuda' # device
+    compile: bool = True # use PyTorch 2.0 to compile the model to be faster
+    config_file: str = None # config file to override default
+
+
+globals().update(vars(Args())) # sic
+if config_file is not None:
+    exec(open(config_file).read())
+
 ddp = int(os.environ.get('LOCAL_RANK', -1)) != -1 # is this a ddp run?
 if ddp:
     init_process_group(backend=backend)
