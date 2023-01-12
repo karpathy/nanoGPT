1467ac1eb363bab3c19b479e74f9ef02e9413789
Thomas Vogt <mehrmorgen@me.com>
2023-01-12 06:02:50 +0000

    inference time mini-optimization low-hanging fruit ty @jxtps for raising: when we are running inference we can apply lm_head on only the very last token
    (cherry picked from commit ba07be78384700499ddfb0e86816b6a5e3db5648)

--- train.py
+++ train.py
@@ -130,9 +130,8 @@ def get_batch(split):
 iter_num = 0
 best_val_loss = 1e9
 
-# model init
-model_args = dict(n_layer=n_layer, n_head=n_head, n_embd=n_embd, block_size=block_size,
-                  bias=bias, vocab_size=None, dropout=dropout) # start with model_args from command line
+# model init. TODO: fix bug we should also propagate the correct vocab_size to the model_args
+model_args = dict(n_layer = n_layer, n_head = n_head, n_embd = n_embd, block_size = block_size, dropout = dropout)
 if init_from == 'scratch':
     # init a new model from scratch
     print("Initializing a new model from scratch")
