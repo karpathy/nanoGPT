91d02510ce8c832923d0a6408d983e2d55f0291e
Andrej Karpathy <andrej.karpathy@gmail.com>
2023-01-14 03:57:00 +0000

    fix bug... if topk > vocab_size, torch.topk will throw error

--- model.py
+++ model.py
@@ -277,7 +277,7 @@ class GPT(nn.Module):
             logits = logits[:, -1, :] / temperature
             # optionally crop the logits to only the top k options
             if top_k is not None:
-                v, _ = torch.topk(logits, top_k)
+                v, _ = torch.topk(logits, min(top_k, logits.size(-1)))
                 logits[logits < v[:, [-1]]] = -float('Inf')
             # apply softmax to convert logits to (normalized) probabilities
             probs = F.softmax(logits, dim=-1)
