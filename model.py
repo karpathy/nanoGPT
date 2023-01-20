8331916eee705254d7df7584989e8140222eb019
kucicm <10778553+KucicM@users.noreply.github.com>
2023-01-20 18:11:26 +0100

    inplace add

--- model.py
+++ model.py
@@ -86,8 +86,8 @@ class Block(nn.Module):
         self.mlp = MLP(config)
 
     def forward(self, x):
-        x = x + self.attn(self.ln_1(x))
-        x = x + self.mlp(self.ln_2(x))
+        x += self.attn(self.ln_1(x))
+        x += self.mlp(self.ln_2(x))
         return x
 
 @dataclass
