3bb67258557d38409fbb6848fec4e23403c02bf2
Pierre Bhat <123220777+pierrebhat@users.noreply.github.com>
2023-01-21 20:22:28 -0800

    Updated data/shakespeare_char/prepare.py

--- data/shakespeare_char/prepare.py
+++ data/shakespeare_char/prepare.py
@@ -34,8 +34,9 @@ def decode(l):
     ''.join([itos[i] for i in l]) # decoder: take a list of integers, output a string
 
 # create the train and test splits
-split_index = int(len(data) * 0.9) # add this line 
-train_data, val_data = data[:split_index], data[split_index:] # change this line
+n = len(data)
+train_data = data[:int(n*0.9)]
+val_data = data[int(n*0.9):]
 
 # encode both to integers
 train_ids = encode(train_data)
@@ -44,8 +45,8 @@ print(f"train has {len(train_ids):,} tokens")
 print(f"val has {len(val_ids):,} tokens")
 
 # export to bin files
-train_ids = np.array(train_ids, dtype=np.uint16)
-val_ids = np.array(val_ids, dtype=np.uint16)
+train_ids = np.array(train_ids, dtype=np.int32)
+val_ids = np.array(val_ids, dtype=np.int32)
 train_ids.tofile(os.path.join(os.path.dirname(__file__), 'train.bin'))
 val_ids.tofile(os.path.join(os.path.dirname(__file__), 'val.bin'))
 
