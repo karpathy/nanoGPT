a41132a1d4d2990f2f69135988d6afa1a9af5bf6
Pierre Bhat <123220777+pierrebhat@users.noreply.github.com>
2023-01-21 20:31:34 -0800

    Updated data/openwebtext/prepare.py

--- data/openwebtext/prepare.py
+++ data/openwebtext/prepare.py
@@ -34,10 +34,9 @@ split_dataset['val'] = split_dataset.pop('test') # rename the test split to val
 # we now want to tokenize the dataset. first define the encoding function (gpt2 bpe)
 enc = tiktoken.get_encoding("gpt2")
 def process(example):
-    ids = enc.encode_ordinary(example['text']) # encode_ordinary ignores any special tokens    
-    # note: I think eot should be prepended not appended... hmm. it's called "eot" though...    
-    ids.insert(0, enc.eot_token) # add the end of text token, e.g. 50256 for gpt2 bpe    
-    out = {'ids': ids, 'len': len(ids)}    
+    ids = enc.encode_ordinary(example['text']) # encode_ordinary ignores any special tokens
+    ids.prepend(enc.eot_token) # add the end of text token, e.g. 50256 for gpt2 bpe
+    out = {'ids': ids, 'len': len(ids)}
     return out
 
 # tokenize the dataset
