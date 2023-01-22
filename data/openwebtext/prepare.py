9c2591d2829de32d775d9bed19a30011bf6edb72
Pierre Bhat <123220777+pierrebhat@users.noreply.github.com>
2023-01-21 20:04:00 -0800

    Updated prepare.py

--- data/openwebtext/prepare.py
+++ data/openwebtext/prepare.py
@@ -35,8 +35,7 @@ split_dataset['val'] = split_dataset.pop('test') # rename the test split to val
 enc = tiktoken.get_encoding("gpt2")
 def process(example):
     ids = enc.encode_ordinary(example['text']) # encode_ordinary ignores any special tokens
-    ids.append(enc.eot_token) # add the end of text token, e.g. 50256 for gpt2 bpe
-    # note: I think eot should be prepended not appended... hmm. it's called "eot" though...
+    ids.insert(0, enc.eot_token) # insert the end of text token at the start of the list, e.g. 50256 for gpt2 bpe    
     out = {'ids': ids, 'len': len(ids)}
     return out
 
