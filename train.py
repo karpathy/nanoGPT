ab26415452b69685679b4877065d226ba10e3ea7
Thomas Vogt <mehrmorgen@me.com>
2023-01-27 20:51:50 +0000

    stay true to the README file and set grad accum to 5, so the default batch size is about 0.5M and is reproducing gpt2
    (cherry picked from commit 96b2b6cb71f7f16e441ff7838bad9b4b89993cab)

--- train.py
+++ train.py
@@ -45,7 +45,7 @@ wandb_project = 'owt'
 wandb_run_name = 'gpt2' # 'run' + str(time.time())
 # data
 dataset = 'openwebtext'
-gradient_accumulation_steps = 1 # used to simulate larger batch sizes
+gradient_accumulation_steps = 5 # used to simulate larger batch sizes
 batch_size = 12 # if gradient_accumulation_steps > 1, this is the micro-batch size
 block_size = 1024
 # model
