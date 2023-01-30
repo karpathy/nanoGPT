263b800d556b5157d902ed95baeda1c07ac992cf
Phillip Lakaschus <Philliplakaschus@gmail.com>
2023-01-30 20:26:13 +0100

    Select smaller openwebtext dataset

--- data/openwebtext/prepare.py
+++ data/openwebtext/prepare.py
@@ -9,13 +9,13 @@ from datasets import load_dataset # huggingface datasets
 
 # number of workers in .map() call
 # good number to use is ~order number of cpu cores // 2
-num_proc = 8
+num_proc = 1
 
 # takes 54GB in huggingface .cache dir, about 8M documents (8,013,769)
-dataset = load_dataset("openwebtext")
+dataset = load_dataset("stas/openwebtext-10k")
 
 # owt by default only contains the 'train' split, so create a test split
-split_dataset = dataset["train"].train_test_split(test_size=0.0005, seed=2357, shuffle=True)
+split_dataset = dataset["train"].train_test_split(test_size=0.1, seed=2351, shuffle=True)
 split_dataset['val'] = split_dataset.pop('test') # rename the test split to val
 
 # this results in:
