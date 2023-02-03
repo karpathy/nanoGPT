f94364ac332d4b0b084124e32ff476129ae9c1ad
Thomas Vogt <mehrmorgen@me.com>
2023-02-03 22:14:37 +0000

    add gpt2 training config
    (cherry picked from commit 1904a0dea93d96f0f8fef80e2cd61a256482cd82)

--- config/train_gpt2.py
+++ config/train_gpt2.py
@@ -1,6 +1,4 @@
 # config for training GPT-2 (124M) down to very nice loss of ~2.85 on 1 node of 8X A100 40GB
-# launch as the following (e.g. in a screen session) and wait ~5 days:
-# $ torchrun --standalone --nproc_per_node=8 train.py config/train_gpt2.py
 
 wandb_log = True
 wandb_project = 'owt'
@@ -10,7 +8,7 @@ wandb_run_name='gpt2-124M'
 # 12 batch size * 1024 block size * 5 gradaccum * 8 GPUs = 491,520
 batch_size = 12
 block_size = 1024
-gradient_accumulation_steps = 5 * 8
+gradient_accumulation_steps = 5
 
 # this makes total number of tokens be 300B
 max_iters = 600000
