b8286f343e2c93eb87133a2ffeb470f08bf780da
Nan Yang <yangnan719@gmail.com>
2023-02-04 11:16:26 -0800

    Pin memory only when training on GPU

--- train.py
+++ train.py
@@ -113,7 +113,12 @@ def get_batch(split):
     x = torch.stack([torch.from_numpy((data[i:i+block_size]).astype(np.int64)) for i in ix])
     y = torch.stack([torch.from_numpy((data[i+1:i+1+block_size]).astype(np.int64)) for i in ix])
     # pin arrays x,y, which allows us to move them to GPU asynchronously (non_blocking=True)
-    x, y = x.pin_memory().to(device, non_blocking=True), y.pin_memory().to(device, non_blocking=True)
+    if "cuda" in device:
+        # GPU training
+        x, y = x.pin_memory().to(device, non_blocking=True), y.pin_memory().to(device, non_blocking=True)
+    else:
+        # CPU or MPS training
+        x, y = x.to(device), y.to(device)
     return x, y
 
 # init these up here, can override if init_from='resume' (i.e. from a checkpoint)
