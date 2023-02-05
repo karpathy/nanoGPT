580902617c329dd9f0978606caa2db85839de906
Andrej Karpathy <andrej.karpathy@gmail.com>
2023-02-05 00:43:15 +0000

    oops optimizer now demands to know device_type

--- bench.py
+++ bench.py
@@ -57,7 +57,7 @@ gptconf = GPTConfig(
 model = GPT(gptconf)
 model.to(device)
 
-optimizer = model.configure_optimizers(weight_decay=1e-2, learning_rate=1e-4, betas=(0.9, 0.95))
+optimizer = model.configure_optimizers(weight_decay=1e-2, learning_rate=1e-4, betas=(0.9, 0.95), device_type=device_type)
 
 if compile:
     print("Compiling model...")
