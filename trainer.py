9f4270b39657381cd256e6dc4e8e6c3f17fa39eb
Sanjeevan Ahilan <sanjeevanahilan@gmail.com>
2023-02-14 22:09:05 +0000

    selfs added

--- trainer.py
+++ trainer.py
@@ -134,7 +134,7 @@ class Trainer():
         elif self.init_from == 'resume':
             print(f"Resuming training from {self.out_dir}")
             # resume training from a checkpoint.
-            ckpt_path = os.path.join(out_dir, 'ckpt.pt')
+            ckpt_path = os.path.join(self.out_dir, 'ckpt.pt')
             checkpoint = torch.load(ckpt_path, map_location=self.device)
             checkpoint_model_args = checkpoint['model_args']
             # force these config attributes to be equal otherwise we can't even resume training
@@ -155,9 +155,9 @@ class Trainer():
             iter_num = checkpoint['iter_num']
             best_val_loss = checkpoint['best_val_loss']
         elif self.init_from.startswith('gpt2'):
-            print(f"Initializing from OpenAI GPT-2 weights: {init_from}")
+            print(f"Initializing from OpenAI GPT-2 weights: {self.init_from}")
             # initialize from OpenAI GPT-2 weights
-            override_args = dict(dropout=dropout)
+            override_args = dict(dropout=self.dropout)
             model = GPT.from_pretrained(init_from, override_args)
             # read off the created config params, so we can store them into checkpoint correctly
             for k in ['n_layer', 'n_head', 'n_embd', 'block_size', 'bias', 'vocab_size']:
