57cf0766980a7c97296522bf8f7ebab36ea73ca7
Kirill <iam@python273.pw>
2023-03-24 14:51:02 +0300

    Fix GPT.crop_block_size when flash attention is available

--- model.py
+++ model.py
@@ -207,7 +207,8 @@ class GPT(nn.Module):
         self.config.block_size = block_size
         self.transformer.wpe.weight = nn.Parameter(self.transformer.wpe.weight[:block_size])
         for block in self.transformer.h:
-            block.attn.bias = block.attn.bias[:,:,:block_size,:block_size]
+            if hasattr(block.attn, 'bias'):
+                block.attn.bias = block.attn.bias[:,:,:block_size,:block_size]
 
     @classmethod
     def from_pretrained(cls, model_type, override_args=None):
