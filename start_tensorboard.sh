a80147cf6d1850384b8db761806c314ffd1ad940
Gregory Kielian <gkielian@google.com>
2023-10-18 22:58:53 +0000

    Add tensorboard logging to train.py file
    - Added a helper script for tensorboard logger.
    - Added tensorboard logging with train and val loss on same graph.
    - Modified modulo to match toy example in the modulo config.
    - Added feature for saving checkpoint only at the end (helps prevent
    excessive checkpoint saves when doing frequent validation loss
    logging)

---
+++ start_tensorboard.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# Note: may need to run this script as `source start_tensorboard.sh` to utilize environment
+
+tensorboard --logdir=./logs # creates and starts logging logs into the logs folder
