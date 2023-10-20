f9ccb619d68b9ce87fb80e07b670cf9a313f5189
Gregory Kielian <gkielian@google.com>
2023-10-20 01:09:09 +0000

    Add script for translating dataset in json format

--- data_augmentation/get_ts_dataset.sh
+++ data_augmentation/get_ts_dataset.sh
@@ -5,35 +5,36 @@
 datasets_root_folder="datasets"
 json_folder="json_stories"
 txt_folder="txt_stories"
+archive_folder="archives"
 
 # Create necessary directory structure
 if [[ ! -d "${datasets_root_folder}" ]]; then
-  mkdir -p "${datasets_root_folder}"
   mkdir -p "${datasets_root_folder}"
 fi
 
 if [[ ! -d "${json_folder}" ]]; then
   mkdir -p "${datasets_root_folder}/${json_folder}"
-  mkdir -p "${datasets_root_folder}/${json_folder}"
 fi
 
 if [[ ! -d "${txt_folder}" ]]; then
   mkdir -p "${datasets_root_folder}/${txt_folder}"
-  mkdir -p "${datasets_root_folder}/${txt_folder}"
+fi
+
+if [[ ! -d "${archive_folder}" ]]; then
+  mkdir -p "${datasets_root_folder}/${archive_folder}"
 fi
 
 # Obtain the files if they don't already exist
-wget -nc -P "${datasets_root_folder}" https://huggingface.co/datasets/roneneldan/TinyStories/resolve/main/TinyStoriesV2-GPT4-train.txt
-wget -nc -P "${datasets_root_folder}" https://huggingface.co/datasets/roneneldan/TinyStories/resolve/main/TinyStoriesV2-GPT4-valid.txt
-wget -nc -P "${datasets_root_folder}/${json_folder}" https://huggingface.co/datasets/roneneldan/TinyStories/resolve/main/TinyStories_all_data.tar.gz
+wget -nc -P "${datasets_root_folder}/${txt_folder}" https://huggingface.co/datasets/roneneldan/TinyStories/resolve/main/TinyStoriesV2-GPT4-train.txt
+wget -nc -P "${datasets_root_folder}/${txt_folder}" https://huggingface.co/datasets/roneneldan/TinyStories/resolve/main/TinyStoriesV2-GPT4-valid.txt
+wget -nc -P "${datasets_root_folder}/${archive_folder}" https://huggingface.co/datasets/roneneldan/TinyStories/resolve/main/TinyStories_all_data.tar.gz
 
-# subshell for the json extraction
-(
-cd "${datasets_root_folder}/${json_folder}"
-if [ -f "data49.json" ]; then
+if [ -f  "${datasets_root_folder}/${json_folder}/data49.json" ]; then
   exit 0
 else
-  tar xvfz TinyStories_all_data.tar.gz
+  # subshell for the json extraction
+  (
+  cd "${datasets_root_folder}/${archive_folder}"
+  tar xvfz TinyStories_all_data.tar.gz -C "../${json_folder}"
+  )
 fi
-)
-
