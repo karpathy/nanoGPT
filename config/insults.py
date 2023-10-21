1f7cd2061e4986040c98bb0c378309026e2ebbb1
Chris Kerwell Gresla <ckgresla@gmail.com>
2023-10-21 23:14:08 +0000

    configuration file for insult job runs of the trainer

---
+++ config/insults.py
@@ -0,0 +1,53 @@
+# Custom Config for the Perplexity blog Work
+
+
+# I/O
+out_dir = 'insulted'
+eval_interval = 200
+log_interval = 10
+eval_iters = 200
+eval_only = False # if True, script exits right after the first eval
+always_save_checkpoint = True # if True, always save a checkpoint after each eval
+init_from = 'scratch' # 'scratch' or 'resume' or 'gpt2*'
+
+# wandb logging
+# TODO@ckg: set up for logging training runs?
+wandb_log = False # disabled by default
+wandb_project = 'owt'
+wandb_run_name = 'gpt2' # 'run' + str(time.time())
+
+# data
+dataset = 'shakespeare_insults'
+gradient_accumulation_steps = 5 * 8 # used to simulate larger batch sizes
+batch_size = 42 # if gradient_accumulation_steps > 1, this is the micro-batch size
+block_size = 64
+vocab_size = 328
+
+# model
+n_layer = 12
+n_head = 8
+n_embd = 784
+dropout = 0.0 # for pretraining 0 is good, for finetuning try 0.1+
+bias = False # do we use bias inside LayerNorm and Linear layers?
+# meta_vocab_size = 328 #hardcoded from the insults data preparator --> custom vocab dim!
+
+# adamw optimizer
+learning_rate = 6e-4 # max learning rate
+max_iters = 600000 # total number of training iterations
+weight_decay = 1e-1
+beta1 = 0.9
+beta2 = 0.95
+grad_clip = 1.0 # clip gradients at this value, or disable if == 0.0
+
+# learning rate decay settings
+decay_lr = True # whether to decay the learning rate
+warmup_iters = 2000 # how many steps to warm up for
+lr_decay_iters = 600000 # should be ~= max_iters per Chinchilla
+min_lr = 6e-5 # minimum learning rate, should be ~= learning_rate/10 per Chinchilla
+
+# DDP settings
+backend = 'nccl' # 'nccl', 'gloo', etc.
+
+# system
+device = 'cuda' # examples: 'cpu', 'cuda', 'cuda:0', 'cuda:1' etc., or try 'mps' on macbooks
+compile = True # use PyTorch 2.0 to compile the model to be faster
--- data/shakespeare_insults/prepare.py
+++ data/shakespeare_insults/prepare.py
@@ -4,6 +4,7 @@ from typing import Any, List, Dict, Union
 import pickle
 import numpy as np
 
+
 # this dataset comes for FREE inside this repository, that is a pretty good deal!
 insults = ["A most notable coward, an infinite and endless liar, an hourly promise breaker, the owner of no one good quality.", "Away, you starvelling, you elf-skin, you dried neat’s-tongue, bull’s-pizzle, you stock-fish!", "Away, you three-inch fool! ", "Come, come, you froward and unable worms!", "Go, prick thy face, and over-red thy fear, Thou lily-liver’d boy.", "His wit’s as thick as a Tewkesbury mustard.", "I am pigeon-liver’d and lack gall.", "I am sick when I do look on thee", "I must tell you friendly in your ear, sell when you can, you are not for all markets", "If thou wilt needs marry, marry a fool; for wise men know well enough what monsters you make of them", "I’ll beat thee, but I would infect my hands", "I scorn you, scurvy companion.", "Methink’st thou art a general offence and every man should beat thee", "More of your conversation would infect my brain", "My wife’s a hobby horse", "Peace, ye fat guts", "Aroint thee: go away, rump-fed runion: slu", "The rankest compound of villainous smell that ever offended nostri", "The tartness of his face sours ripe grapes", "There’s no more faith in thee than in a stewed prune", "Thine forward voice, now, is to speak well of thine friend; thine backward voice is to utter foul speeches and to detract", "That trunk of humours, that bolting-hutch of beastliness, that swollen parcel of dropsies, that huge bombard of sack, that stuffed ", "cloak-bag of guts", "that roasted Manningtree ox with pudding in his belly", "that reverend vice" "Thine face is not worth sunburning", "This woman’s an easy glove, my lord, she goes off and on at pleasure", "Thou art a boil, a plague sor", "Was the Duke a flesh-monger, a fool and a coward", "Thou art as fat as butter", "Here is the babe, as loathsome as a toad", "Like the toad; ugly and venomous", "Thou art unfit for any place but hell", "Thou cream faced loo", "Thou clay-brained guts, thou knotty-pated fool, thou whoreson obscene greasy tallow-catch", "Thou damned and luxurious mountain goat", "Thou elvish-mark’d, abortive, rooting hog", "Thou leathern-jerkin, crystal-button, knot-pated, agatering, puke-stocking, caddis-garter, smooth-tongue, Spanish pouch", "Thou lump of foul deformit", "That poisonous bunch-back’d toad", "Thou sodden-witted lord! Thou hast no more brain than I have in mine elbows", "Thou subtle, perjur’d, false, disloyal man", "Thou whoreson zed , thou unnecessary letter", "Thy sin’s not accidental, but a trade", "Thy tongue outvenoms all the worms of Nile", "Would thou wert clean enough to spit upo", "Would thou wouldst burst", "You poor, base, rascally, cheating lack-linen mate!", "You are as a candle, the better burnt out", "You scullion! You rampallian! You fustilarian! I’ll tickle your catastrophe", "You starvelling, you eel-skin, you dried neat’s-tongue, you bull’s-pizzle, you stock-fish–O for breath to utter what is like ", "Your brain is as dry as the remainder biscuit after voyage", "Virginity breeds mites, much like a cheese", "Villain, I have done thy mothe", "Heaven truly knows that thou art false as hel", "Out of my sight! Thou dost infect mine eyes", "No longer from head to foot than from hip to hip, she is spherical, like a globe; I could find countries in her", "You have such a February face, So full of frost, of storm, and cloudiness"]
 
@@ -123,7 +124,7 @@ print(f"train has {len(train_ids):,} instances with {total_training_toks} tokens
 print(f"val has {len(val_ids):,} instances with {total_val_toks} tokens")
 
 
-# FIND THE MAX SEQLEN 
+# FIND THE MAX SEQLEN
 results = map(tokenizer.__call__, insults)
 m = 0
 for i in results:
@@ -131,7 +132,7 @@ for i in results:
     m = max(m, len(i))
 print("longest seqlen in dataset: ", m, "w/o padding...")
 print(f"vocabulary size: {tokenizer.vocab_dim}")
-    
+
 
 # export the tokenized data to bin files
 train_ids = np.array(train_ids, dtype=np.uint16)
