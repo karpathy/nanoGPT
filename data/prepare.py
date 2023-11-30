b47c6a542d2122ad2dcfbd6855d5e193a64eb286
Sam Foreman <saforem2@gmail.com>
2023-11-29 21:31:50 -0600

    Add `data/prepare.py`

---
+++ data/prepare.py
@@ -0,0 +1,113 @@
+"""
+prepare.py
+"""
+from __future__ import absolute_import, annotations, division, print_function
+# saves a HuggingFace dataset to a binary file for training. following was helpful:
+# https://github.com/HazyResearch/flash-attention/blob/main/training/src/datamodules/language_modeling_hf.py
+
+import os
+from tqdm import tqdm
+import numpy as np
+import tiktoken
+from datasets import load_dataset # huggingface datasets
+from pathlib import Path
+from rich import print
+
+# HF_DATASETS_CACHE.mkdir(parents=True, exist_ok=True)
+# os.environ['HF_DATASETS_CACHE'] = HF_DATASETS_CACHE.as_posix()
+# print(f'Using HF_DATASETS_CACHE={HF_DATASETS_CACHE.as_posix()}')
+
+HF_DATASETS_CACHE = os.environ.get('HF_DATASETS_CACHE', None)
+if HF_DATASETS_CACHE is None:
+    HERE = Path(os.path.abspath(__file__)).parent
+    HF_DATASETS_CACHE = HERE / ".cache" / "huggingface"
+    print(f'Setting {HF_DATASETS_CACHE.as_posix()=}')
+    os.environ['HF_DATASETS_CACHE'] = HF_DATASETS_CACHE.as_posix()
+else:
+    print(f'Caught {HF_DATASETS_CACHE.as_posix()=} from env')
+
+# number of workers in .map() call
+# good number to use is ~order number of cpu cores // 2
+num_proc = 8
+
+# number of workers in load_dataset() call
+# best number might be different from num_proc above as it also depends on NW speed.
+# it is better than 1 usually though
+num_proc_load_dataset = num_proc
+# HF_DATASETS_CACHE="./cache"
+
+
+if __name__ == '__main__':
+    import sys
+    args = sys.argv[1:]
+    assert len(args) > 0, 'Expected name of dataset'
+    name = args[0]
+    if len(args) > 1:
+        data_dir = args[1]
+    else:
+        data_dir = os.environ.get('OUTDIR', os.path.dirname(__file__))
+
+    dataset = load_dataset(name)
+
+    # owt by default only contains the 'train' split, so create a test split
+    # split_dataset = dataset["train"].train_test_split(test_size=0.1, seed=2357, shuffle=True)
+    # split_dataset['val'] = split_dataset.pop('test') # rename the test split to val
+
+    # this results in:
+    # >>> split_dataset
+    # DatasetDict({
+    #     train: Dataset({
+    #         features: ['text'],
+    #         num_rows: 8009762
+    #     })
+    #     val: Dataset({
+    #         features: ['text'],
+    #         num_rows: 4007
+    #     })
+    # })
+    outdir = Path(data_dir).joinpath(name)
+    # outdir = Path(os.path.dirname(__file__)).joinpath(name)
+    outdir.mkdir(exist_ok=True, parents=True)
+    print(f'Saving dataset {name} to {outdir}')
+    # takes 54GB in huggingface .cache dir, about 8M documents (8,013,769)
+
+    # we now want to tokenize the dataset. first define the encoding function (gpt2 bpe)
+    enc = tiktoken.get_encoding("gpt2")
+    def process(example):
+        ids = enc.encode_ordinary(example['text']) # encode_ordinary ignores any special tokens
+        ids.append(enc.eot_token) # add the end of text token, e.g. 50256 for gpt2 bpe
+        # note: I think eot should be prepended not appended... hmm. it's called "eot" though...
+        out = {'ids': ids, 'len': len(ids)}
+        return out
+
+    # tokenize the dataset
+    tokenized = split_dataset.map(
+        process,
+        remove_columns=['text'],
+        desc="tokenizing the splits",
+        num_proc=num_proc,
+    )
+
+    # concatenate all the ids in each dataset into one large file we can use for training
+    # train.bin is ~17GB, val.bin ~8.5MB
+    # train has ~9B tokens (9,035,582,198)
+    # val has ~4M tokens (4,434,897)
+
+    # to read the bin files later, e.g. with numpy:
+    # m = np.memmap('train.bin', dtype=np.uint16, mode='r')
+    for split, dset in tokenized.items():
+        arr_len = np.sum(dset['len'], dtype=np.uint64)
+        # filename = os.path.join(os.path.dirname(__file__), f'{split}.bin')
+        filename = outdir.joinpath(f'{split}.bin')
+        dtype = np.uint16 # (can do since enc.max_token_value == 50256 is < 2**16)
+        arr = np.memmap(filename.as_posix(), dtype=dtype, mode='w+', shape=(arr_len,))
+        total_batches = 1024
+        idx = 0
+        for batch_idx in tqdm(range(total_batches), desc=f'writing {filename}'):
+            # Batch together samples for faster write
+            batch = dset.shard(num_shards=total_batches, index=batch_idx, contiguous=True).with_format('numpy')
+            arr_batch = np.concatenate(batch['ids'])
+            # Write into mmap
+            arr[idx : idx + len(arr_batch)] = arr_batch
+            idx += len(arr_batch)
+        arr.flush()
