112f65ab2c5ec595219bee51d8c34450a704d2a2
kassi-bertrand <bertran.billy@gmail.com>
2024-01-05 07:26:27 -0600

    Decode tokens one at a time, instead of printing them all at once.

--- sample.py
+++ sample.py
@@ -7,6 +7,7 @@ from contextlib import nullcontext
 import torch
 import tiktoken
 from model import GPTConfig, GPT
+from time import sleep 
 
 # -----------------------------------------------------------------------------
 init_from = 'resume' # either 'resume' (from an out_dir) or a gpt2 variant (e.g. 'gpt2-xl')
@@ -85,5 +86,13 @@ with torch.no_grad():
     with ctx:
         for k in range(num_samples):
             y = model.generate(x, max_new_tokens, temperature=temperature, top_k=top_k)
-            print(decode(y[0].tolist()))
+            
+            # Decode output tokens, and print them one at a time.
+            token_ids = y[0].tolist()
+            for token_id in token_ids:
+                token_txt = decode([token_id])
+                print(token_txt, end='', flush=True)
+                sleep(0.1)
+
+            print()
             print('---------------')
