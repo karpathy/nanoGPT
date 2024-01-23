e329791888df41af05d22219215f4b935b6c638e
MarcoMueglich <marco.mueglich@gmail.com>
2024-01-23 09:22:01 +0100

    added qnn QuantMultiHeadAttention

--- model.py
+++ model.py
@@ -78,65 +78,6 @@ class CausalSelfAttention(nn.Module):
         y = self.resid_dropout(self.c_proj(y))
         return y
 
-class QuantSelfAttention(nn.Module):
-
-    def __init__(self, config):
-        super().__init__()
-        assert config.n_embd % config.n_head == 0
-        # key, query, value projections for all heads, but in a batch
-        self.c_attn = qnn.QuantLinear(config.n_embd, 3 * config.n_embd, bias=config.bias, bias_quant=Int32Bias, weight_bit_width=config.weight_bit_width)
-        # output projection
-        self.c_proj = qnn.QuantLinear(config.n_embd, config.n_embd, bias=config.bias, bias_quant=Int32Bias, weight_bit_width=config.weight_bit_width)
-        # regularization
-        self.attn_dropout = qnn.QuantDropout(config.dropout)
-        self.resid_dropout = qnn.QuantDropout(config.dropout)
-
-        self.multi_head_attention = qnn.QuantMultiheadAttention(config.n_embd, config.n_head)
-
-        self.n_head = config.n_head
-        self.n_embd = config.n_embd
-        self.dropout = config.dropout
-        # flash attention make GPU go brrrrr but support is only in PyTorch >= 2.0
-        self.flash = hasattr(torch.nn.functional, 'scaled_dot_product_attention')
-        if not self.flash:
-            print("WARNING: using slow attention. Flash Attention requires PyTorch >= 2.0")
-            # causal mask to ensure that attention is only applied to the left in the input sequence
-            self.register_buffer("bias", torch.tril(torch.ones(config.block_size, config.block_size))
-                                        .view(1, 1, config.block_size, config.block_size))
-
-    def forward(self, x):
-        B, T, C = x.size() # batch size, sequence length, embedding dimensionality (n_embd)
-
-        # calculate query, key, values for all heads in batch and move head forward to be the batch dim
-        q, k, v  = self.c_attn(x).split(self.n_embd, dim=2)
-        k = k.view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs)
-        q = q.view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs)
-        v = v.view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs)
-
-        # causal self-attention; Self-attend: (B, nh, T, hs) x (B, nh, hs, T) -> (B, nh, T, T)
-        if self.flash:
-            # efficient attention using Flash Attention CUDA kernels
-            y = torch.nn.functional.scaled_dot_product_attention(q, k, v, attn_mask=None, dropout_p=self.dropout if self.training else 0, is_causal=True)
-        else:
-            # manual implementation of attention
-            att = (q @ k.transpose(-2, -1)) * (1.0 / math.sqrt(k.size(-1)))
-            att = att.masked_fill(self.bias[:,:,:T,:T] == 0, float('-inf'))
-            att = F.softmax(att, dim=-1)
-            att = self.attn_dropout(att)
-            y = att @ v # (B, nh, T, T) x (B, nh, T, hs) -> (B, nh, T, hs)
-
-        # broken for now
-        # y = self.multi_head_attention.multi_head_attention(query=q, key=k, value=v, num_heads=self.n_head, 
-        #                                                    embed_dim_to_check=self.n_embd, bias_k=torch.zeros(self.n_embd), 
-        #                                                    bias_v=torch.zeros(self.n_embd), dropout_p=self.dropout, add_zero_attn=False)
-        
-        y = y.transpose(1, 2).contiguous().view(B, T, C) # re-assemble all head outputs side by side
-
-        # output projection
-        y = self.resid_dropout(self.c_proj(y))
-        return y
-
-
 class MLP(nn.Module):
 
     def __init__(self, config):
@@ -175,12 +116,13 @@ class Block(nn.Module):
     def __init__(self, config):
         super().__init__()
         self.ln_1 = LayerNorm(config.n_embd, bias=config.bias)
-        self.attn = QuantSelfAttention(config)
+        self.attn = qnn.QuantMultiheadAttention(embed_dim=config.n_embd, num_heads=config.n_head)
         self.ln_2 = LayerNorm(config.n_embd, bias=config.bias)
         self.mlp = QuantMLP(config)
 
     def forward(self, x):
-        x = x + self.attn(self.ln_1(x))
+        normalized_x = self.ln_1(x)
+        x = x + self.attn(query=normalized_x, key=normalized_x, value=normalized_x)[0]
         x = x + self.mlp(self.ln_2(x))
         return x
 
