3e3f86c5b28c6bcad19baf34ac47969cc28cb1e9
howaboutyu <>
2024-02-21 08:04:13 +0000

    always resume

--- config/train_enwik8_char.py
+++ config/train_enwik8_char.py
@@ -32,6 +32,7 @@ beta2 = 0.99 # make a bit bigger because number of tokens per iter is small
 
 warmup_iters = 400 # not super necessary potentially
 
+init_from = 'resume'
 # on macbook also add
 # device = 'cpu'  # run on cpu only
 # compile = False # do not torch compile the model
