919a70d29686e48cb324f4a3b1e8fde305951665
Gregory Kielian <gkielian@google.com>
2024-03-03 21:52:41 -0800

    Add fallback for tensorboard command
    This allows an alternative invocation in case the conventional command
    down not work.

--- start_tensorboard.sh
+++ start_tensorboard.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Note: may need to run this script as `source start_tensorboard.sh` to utilize environment
 
-tensorboard --logdir=./logs # creates and starts logging logs into the logs folder
+tensorboard --logdir=./logs || python3 -m tensorboard.main --logdir=./logs
