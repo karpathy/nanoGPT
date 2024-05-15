e3ed5d1fde1e7a95f5302a106eb7b585d9fc6029
Gregory Kielian <gkielian@google.com>
2024-05-14 23:43:50 -0700

    Remove stray line in Strongermax

--- variations/softmax_variations.py
+++ variations/softmax_variations.py
@@ -117,7 +117,6 @@ class Strongermax(nn.Module):
         self.sum_to_1 = config.strongermax_sum_to_1
         self.divisor = config.strongermax_divisor
         self.div_by_seq_len = config.div_by_seq_len
-        return e_x / divisor
 
     def forward(self, x):
         if self.subtract_max:
