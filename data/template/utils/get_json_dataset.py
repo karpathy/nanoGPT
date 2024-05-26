2badb65ef5ab3af9015f2f9d9dd36c834e64facb
klei22 <kauna@avalonwest.tech>
2024-05-25 23:46:08 -0700

    Format the get_json_dataset.py

--- data/template/utils/get_json_dataset.py
+++ data/template/utils/get_json_dataset.py
@@ -23,7 +23,9 @@ def download_file(url, filename):
         print(f"Downloaded {filename}")
 
 
-def emit_json_contents(json_path, output_text_file, include_keys, value_prefixes, required_key, skip_empty):
+def emit_json_contents(
+    json_path, output_text_file, include_keys, value_prefixes, required_key, skip_empty
+):
     print(f"Emitting contents from {json_path} to {output_text_file}")
     with open(json_path, "r") as f:
         data = json.load(f)
@@ -39,8 +41,7 @@ def emit_json_contents(json_path, output_text_file, include_keys, value_prefixes
                 print(f"Skipping item due to empty required key: {required_key}")
                 continue
             for key, prefix in zip(include_keys, value_prefixes):
-
-                key = key.strip('\'"')
+                key = key.strip("'\"")
                 key = key.rstrip()
 
                 if prev_item_written:
@@ -80,7 +81,15 @@ def concatenate_json_files(json_paths, concatenated_json_path):
         output_file.write("\n]")
 
 
-def main(url, output_text_file, no_output_text, include_keys, value_prefixes, required_key, skip_empty):
+def main(
+    url,
+    output_text_file,
+    no_output_text,
+    include_keys,
+    value_prefixes,
+    required_key,
+    skip_empty,
+):
     file_links = find_file_links(url)
     download_dir = "./downloaded_jsons"
     json_dir = "./json_output"
@@ -99,21 +108,70 @@ def main(url, output_text_file, no_output_text, include_keys, value_prefixes, re
     concatenate_json_files(json_paths, concatenated_json_path)
 
     if not no_output_text:
-        emit_json_contents(concatenated_json_path, output_text_file, include_keys, value_prefixes, required_key, skip_empty)
+        emit_json_contents(
+            concatenated_json_path,
+            output_text_file,
+            include_keys,
+            value_prefixes,
+            required_key,
+            skip_empty,
+        )
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         description="Scrape and convert JSON files from URL and save their contents to a text file."
     )
-    parser.add_argument("--url", type=str, required=True, help="URL to scrape for JSON files.")
-    parser.add_argument("-o", "--output_text_file", type=str, default="input.txt", help="Path to the output text file.")
-    parser.add_argument("--no_output_text", action='store_true', help="Skip creation of output text.")
-    parser.add_argument("-i", "--include_keys", type=str, nargs="+", required=True, help="List of keys to include from the JSON contents.")
-    parser.add_argument("-p", "--value_prefixes", type=str, nargs="+", default=[""], help="List of prefixes to be added to each value when emitting to the text file.")
-    parser.add_argument("-r", "--required_key", type=str, default=None, help="Only emit items that have this key (optional).")
-    parser.add_argument("-s", "--skip_empty", action='store_true', help="Skip any item which is an empty string.")
+    parser.add_argument(
+        "--url", type=str, required=True, help="URL to scrape for JSON files."
+    )
+    parser.add_argument(
+        "-o",
+        "--output_text_file",
+        type=str,
+        default="input.txt",
+        help="Path to the output text file.",
+    )
+    parser.add_argument(
+        "--no_output_text", action="store_true", help="Skip creation of output text."
+    )
+    parser.add_argument(
+        "-i",
+        "--include_keys",
+        type=str,
+        nargs="+",
+        required=True,
+        help="List of keys to include from the JSON contents.",
+    )
+    parser.add_argument(
+        "-p",
+        "--value_prefixes",
+        type=str,
+        nargs="+",
+        default=[""],
+        help="List of prefixes to be added to each value when emitting to the text file.",
+    )
+    parser.add_argument(
+        "-r",
+        "--required_key",
+        type=str,
+        default=None,
+        help="Only emit items that have this key (optional).",
+    )
+    parser.add_argument(
+        "-s",
+        "--skip_empty",
+        action="store_true",
+        help="Skip any item which is an empty string.",
+    )
 
     args = parser.parse_args()
-    main(args.url, args.output_text_file, args.no_output_text, args.include_keys, args.value_prefixes, args.required_key, args.skip_empty)
-
+    main(
+        args.url,
+        args.output_text_file,
+        args.no_output_text,
+        args.include_keys,
+        args.value_prefixes,
+        args.required_key,
+        args.skip_empty,
+    )
