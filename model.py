e9f2f0ccbb3401141d25361940663be5fd447ee9
faresobeid <111092724+faresobeid@users.noreply.github.com>
2024-06-08 22:45:26 +0100

    Update model.py

--- model.py
+++ model.py
@@ -47,30 +47,38 @@ class RWKV_TimeMix_x051a(nn.Module):
             for i in range(config.n_embd):
                 ddd[0, 0, i] = i / config.n_embd
 
+            self.time_maa_x = nn.Parameter(1.0 - torch.pow(ddd, ratio_1_to_almost0))
+            self.time_maa_w = nn.Parameter(1.0 - torch.pow(ddd, ratio_1_to_almost0))
             self.time_maa_k = nn.Parameter(1.0 - torch.pow(ddd, ratio_1_to_almost0))
             self.time_maa_v = nn.Parameter(1.0 - (torch.pow(ddd, ratio_1_to_almost0) + 0.3 * ratio_0_to_1))
             self.time_maa_r = nn.Parameter(1.0 - torch.pow(ddd, 0.5 * ratio_1_to_almost0))
-            self.time_maa_g = nn.Parameter(1.0 - torch.pow(ddd, 0.5 * ratio_1_to_almost0))
 
-            decay_speed = torch.ones(self.n_head)
-            for h in range(self.n_head):
-                decay_speed[h] = -6 + 5 * (h / (self.n_head - 1)) ** (0.7 + 1.3 * ratio_0_to_1)
-            self.time_decay = nn.Parameter(decay_speed.unsqueeze(-1))
-
-            tmp = torch.zeros(self.n_head)
-            for h in range(self.n_head):
-                tmp[h] = ratio_0_to_1 * (1 - (h / (self.n_head - 1)))
-            self.time_faaaa = nn.Parameter(tmp.unsqueeze(-1))
+            D_MIX_LORA = 32 # generate TIME_MIX for w,k,v,r,g
+            self.time_maa_w1 = nn.Parameter(torch.zeros(config.n_embd, D_MIX_LORA*4))
+            self.time_maa_w2 = nn.Parameter(torch.zeros(4, D_MIX_LORA, config.n_embd).uniform_(-0.01, 0.01))
+            
+            D_DECAY_LORA = 64
+            self.time_decay_w1 = nn.Parameter(torch.zeros(config.n_embd, D_DECAY_LORA))
+            self.time_decay_w2 = nn.Parameter(torch.zeros(D_DECAY_LORA, config.n_embd).uniform_(-0.01, 0.01))
+            
+            decay_speed = torch.ones(config.n_embd)
+            for h in range(config.n_embd):
+                decay_speed[h] = -6 + 5 * (h / (config.n_embd - 1)) ** (0.7 + 1.3 * ratio_0_to_1)
+            self.time_decay = nn.Parameter(decay_speed.reshape(1,1,config.n_embd))
+    
+            tmp = torch.zeros(config.n_embd)
+            for h in range(config.n_embd):
+                tmp[h] = ratio_0_to_1 * (1 - (h / (config.n_embd - 1)))
+            self.time_faaaa = nn.Parameter(tmp.reshape(1, self.n_head, 1, 1, self.head_size))
 
         self.time_shift = nn.ZeroPad2d((0, 0, 1, -1))
 
         self.receptance = nn.Linear(config.n_embd, config.n_embd, bias=config.bias)
         self.key = nn.Linear(config.n_embd, config.n_embd, bias=config.bias)
         self.value = nn.Linear(config.n_embd, config.n_embd, bias=config.bias)
-        self.gate = nn.Linear(config.n_embd, config.n_embd, bias=config.bias)
 
         self.output = nn.Linear(config.n_embd, config.n_embd, bias=config.bias)
-        self.ln_x = nn.GroupNorm(self.n_head, config.n_embd, eps=(1e-5)*64)
+        self.ln_x = nn.LayerNorm(config.n_embd)
 
         self.dropout = nn.Dropout(config.dropout)
 
@@ -90,46 +98,51 @@ class RWKV_TimeMix_x051a(nn.Module):
         assert T % Q == 0
 
         xx = self.time_shift(x) - x
-        xk = x + xx * self.time_maa_k
-        xv = x + xx * self.time_maa_v
-        xr = x + xx * self.time_maa_r
-        xg = x + xx * self.time_maa_g
-        r = self.receptance(xr).view(B, T, H, N).transpose(1, 2) # receptance
-        k = self.key(xk).view(B, T, H, N).permute(0, 2, 3, 1) # key
-        v = self.value(xv).view(B, T, H, N).transpose(1, 2) # value
-        g = F.silu(self.gate(xg)) # extra gate
 
-        w = torch.exp(-torch.exp(self.time_decay.float())) # time_decay
+        xxx = x + xx * self.time_maa_x
+        xxx = torch.tanh(xxx @ self.time_maa_w1).view(B*T, 4, -1).transpose(0, 1)
+        xxx = torch.bmm(xxx, self.time_maa_w2).view(4, B, T, -1)
+        mw, mk, mv, mr = xxx.unbind(dim=0)
+
+        xw = x + xx * (self.time_maa_w + mw)
+        xk = x + xx * (self.time_maa_k + mk)
+        xv = x + xx * (self.time_maa_v + mv)
+        xr = x + xx * (self.time_maa_r + mr)
+        r = self.receptance(xr).view(B, T, H, N).transpose(1, 2) # receptance
+        k = self.key(xk).view(B, T, H, N).transpose(1, 2) # key
+        v = self.value(xv).view(B, T, H, N).transpose(1, 2) # value
+
+        w = self.time_decay + torch.tanh(xw @ self.time_decay_w1) @ self.time_decay_w2
+        w = w.view(B, T, H, N).transpose(1, 2).mean(dim=-1)
+        w_log = -torch.exp(w.float()) # time_decay
         u = self.time_faaaa.float() # time_first
 
-        ws = w.pow(Q).view(1, H, 1, 1)
-
-        ind = torch.arange(Q-1, -1, -1, device=r.device).unsqueeze(0).repeat(H, 1)
-        w = w.repeat(1, Q).pow(ind)
-
-        wk = w.view(1, H, 1, Q)
-        wb = wk.transpose(-2, -1).flip(2)
-
-        w = torch.cat([w[:, 1:], u], dim=1)
-        w = F.pad(w, (0, Q))
-        w = torch.tile(w, [Q])
-        w = w[:, :-Q].view(-1, Q, 2*Q - 1)
-        w = w[:, :, Q-1:].view(1, H, Q, Q)
-
-        w = w.to(dtype=r.dtype) # the decay matrix
-        wk = wk.to(dtype=r.dtype)
-        wb = wb.to(dtype=r.dtype)
-        ws = ws.to(dtype=r.dtype)
-
-        state = torch.zeros(B, H, N, N, device=r.device, dtype=r.dtype) # state
-        y = torch.empty(B, H, T, N, device=r.device, dtype=r.dtype) # output
-
-        for i in range(T // Q): # the rwkv-x051a operator
-            rr = r[:, :, i*Q:i*Q+Q, :]
-            kk = k[:, :, :, i*Q:i*Q+Q]
-            vv = v[:, :, i*Q:i*Q+Q, :]
-            y[:, :, i*Q:i*Q+Q, :] = ((rr @ kk) * w) @ vv + (rr @ state) * wb
-            state = ws * state + (kk * wk) @ vv
+        wc_log = w_log.view(B,H,-1,Q,1)
+        wc_log_cum = wc_log.cumsum(dim=-2)
+        shifted_wc_log_cum = F.pad(wc_log_cum, (0, 0, 1, -1))
+        ws = wc_log.sum(dim=-2, keepdim=True)
+        w_inter = ws - wc_log_cum 
+        w_intra = wc_log_cum - wc_log
+        ws = list(ws.exp().to(r.dtype).unbind(dim=-3))
+        w_inter = w_inter.exp().to(r.dtype)
+        w_intra = w_intra.exp().to(r.dtype)
+        w_mask = (shifted_wc_log_cum - wc_log_cum.mT).tril().exp().tril(-1).to(r.dtype)
+        
+        rr = r.view(B,H,-1,Q,N)
+        kk = k.view(B,H,-1,Q,N)
+        vv = v.view(B,H,-1,Q,N)
+        states = []
+        state = torch.zeros(B, H, N, N, device=r.device, dtype=r.dtype) # initial state
+        kv = (kk * w_inter).mT @ vv
+        kv = list(kv.unbind(dim=-3))
+        for i in range(T // Q):
+            states.append(state)
+            state = ws[i] * state + kv[i]
+        states = torch.stack(states,dim=2)
+        inter_chunk = (rr * w_intra) @ states
+        u_diag = torch.einsum('bhntk,bhntk->bhnt', rr, u * kk).diag_embed()
+        intra_chunk = ((rr @ kk.mT) * w_mask + u_diag) @ vv
+        y = inter_chunk + intra_chunk
 
         y = y.transpose(1, 2).contiguous().view(B * T, C)
         y = self.ln_x(y).view(B, T, C) * g
@@ -152,8 +165,8 @@ class RWKV_ChannelMix_x051a(nn.Module):
             self.time_maa_k = nn.Parameter(1.0 - torch.pow(ddd, ratio_1_to_almost0))
             self.time_maa_r = nn.Parameter(1.0 - torch.pow(ddd, ratio_1_to_almost0))
 
-        self.key = nn.Linear(config.n_embd, 3 * config.n_embd, bias=config.bias)
-        self.value = nn.Linear(3 * config.n_embd, config.n_embd, bias=config.bias)
+        self.key = nn.Linear(config.n_embd, int(3.5 * config.n_embd), bias=config.bias)
+        self.value = nn.Linear(int(3.5 * config.n_embd), config.n_embd, bias=config.bias)
         self.receptance = nn.Linear(config.n_embd, config.n_embd, bias=config.bias)
         self.dropout = nn.Dropout(config.dropout)
 
