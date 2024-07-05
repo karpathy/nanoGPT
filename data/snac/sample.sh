584ab7a20eda782d3d47db52b9311a298e159c16
klei22 <kauna@avalonwest.tech>
2024-07-04 22:58:08 -0700

    Add sample.sh and train.sh for easy demo of vc
    This is a simple kind of method for making similarities to one's voice.
    
    We don't yet support starting sentences, but should be doable (might
    need to have a way to get indices directly into sample.py).

--- data/snac/sample.sh
+++ data/snac/sample.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 
+set -x
 
 pushd ../../
-
-train.py --device cuda --dataset snac --shared_fire_embeddings --no-use_abs_pos_embedding --use_fire_embeddings --compile
-
-python3 sample.py --device=cuda --out_dir ./out --start "" --token_boundary $'\n' --num_samples 1 --sample_file ./data/snac/sample.txt
+python3 sample.py --device=cuda --out_dir ./out --start "0" --token_boundary $'\n' --num_samples 1 --sample_file ./data/snac/sample.txt --max_new_tokens 4096
 
 # Remove frames which do not have 7 entries.
 popd
 python3 format.py clean sample.txt output.txt
 
-# Convert into mp3 format for listening
+# # Convert into mp3 format for listening
 python3 snac_converter.py decode output.txt output.mp3 --input_format text
 
--- data/snac/train.sh
+++ data/snac/train.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 pushd ../../
-python3 train.py --device cuda --dataset snac --shared_fire_embeddings --no-use_abs_pos_embedding --use_fire_embeddings --compile --always_save_checkpoint
+python3 train.py --max_iters 10000 --decay_lr --device cuda --dataset snac --shared_fire_embeddings --no-use_abs_pos_embedding --use_fire_embeddings --compile --always_save_checkpoint
 popd
 
