8a85fb6b8183e57f02ebca140a71ac5a97b9e583
P.Y. Laligand <py.laligand@flex.ai>
2024-07-24 12:27:30 +0200

    Allow loading datasets from /input (#1)

--- train.py
+++ train.py
@@ -112,7 +112,9 @@ ptdtype = {'float32': torch.float32, 'bfloat16': torch.bfloat16, 'float16': torc
 ctx = nullcontext() if device_type == 'cpu' else torch.amp.autocast(device_type=device_type, dtype=ptdtype)
 
 # poor man's data loader
-data_dir = os.path.join('data', dataset)
+data_dir = os.path.join('/input', dataset)
+if not os.path.exists(data_dir):
+    data_dir = os.path.join('data', dataset)
 def get_batch(split):
     # We recreate np.memmap every batch to avoid a memory leak, as per
     # https://stackoverflow.com/questions/45132940/numpy-memmap-memory-usage-want-to-iterate-once/61472122#61472122
