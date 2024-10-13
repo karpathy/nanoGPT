dc163242a03986dee5f4c59dcafc2e80ccbeca67
Oren <47992694+OrenLeung@users.noreply.github.com>
2024-10-13 14:21:13 -0400

    Update launch.sh

--- docker/launch.sh
+++ docker/launch.sh
@@ -5,4 +5,4 @@ IT_FLAG=${1:+-t}
 
 docker run --rm ${IT_FLAG:=-it} --gpus all --ipc=host --shm-size=192G \
     -v .:/workspace/llm-train-bench/ \
-    kimbochen/llm-train-bench $CMD
+    llm-train-bench $CMD
