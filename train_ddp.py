becae7a0c3f4c36ce4633d09beba161901f80dcc
Oren <47992694+OrenLeung@users.noreply.github.com>
2024-10-14 16:30:58 -0400

    refactor some stuff (#12)

--- train_ddp.py
+++ train_ddp.py
@@ -1,3 +1,4 @@
+import contextlib
 from dataclasses import asdict
 from pathlib import Path
 
@@ -24,7 +25,6 @@ from transformer_engine.common.recipe import Format, DelayedScaling
 def train(
     cfg_path: str,
     bsz: int = 8,
-    n_workers: int = 8,
     n_steps: int = 128*8,
     grad_acc_steps: int = 8,
     pt_compile: bool = False,
@@ -36,7 +36,6 @@ def train(
     '''
     :param       cfg_path: Model configuration file path
     :param            bsz: Batch size
-    :param      n_workers: Number of CPUs for data loading
     :param        n_steps: Number of training steps
     :param grad_acc_steps: Number of gradient accumulation steps
     :param     pt_compile: Enable PyTorch compile
@@ -48,7 +47,7 @@ def train(
     torch.manual_seed(3985)
     world_size = torch.cuda.device_count()
     train_args = (
-        world_size, cfg_path, bsz, n_workers, n_steps, grad_acc_steps,
+        world_size, cfg_path, bsz, n_steps, grad_acc_steps,
         use_fp8, pt_compile, compile_mode, profile, output_dir
     )
 
@@ -60,7 +59,7 @@ def train(
 
 def train_ddp(
     rank, world_size,
-    cfg_path, bsz, n_workers, n_steps, grad_acc_steps,
+    cfg_path, bsz, n_steps, grad_acc_steps,
     use_fp8, pt_compile, compile_mode, profile, output_dir
 ):
     # Construct process group
@@ -74,12 +73,7 @@ def train_ddp(
     dprint(rank, f'Loaded {model_cls} model.', end=' ')
     cfg_m.estimate_flops_per_token(model, bsz, rank)
 
-    dataset = DummyDataset(cfg_m.vocab_size, cfg_m.max_seq_len, bsz*n_steps)
-    data_loader = DataLoader(
-        dataset, batch_size=bsz,
-        num_workers=n_workers, pin_memory=True, shuffle=False,
-        sampler=DistributedSampler(dataset, rank=rank, num_replicas=world_size, shuffle=True)
-    )
+    data_loader = create_distributed_data_loader(rank, world_size, bsz, n_steps, cfg_m)
     optimizer = torch.optim.AdamW(model.parameters(), fused=True)
     scheduler = torch.optim.lr_scheduler.LambdaLR(optimizer, lambda t: 1.0)
 
@@ -95,9 +89,18 @@ def train_ddp(
         dprint(rank, f'Compiling in {compile_mode} mode')
         model = torch.compile(model, mode=compile_mode)
 
-    # FP8
-    fp8_format = Format.HYBRID  # E4M3 during forward pass, E5M2 during backward pass
-    fp8_recipe = DelayedScaling(fp8_format=fp8_format, amax_history_len=16, amax_compute_algo='max')
+    if use_fp8:
+        # FP8
+        fp8_format = Format.HYBRID  # E4M3 during forward pass, E5M2 during backward pass
+        fp8_recipe = DelayedScaling(fp8_format=fp8_format, amax_history_len=16, amax_compute_algo='max')
+        all_gpus = dist.new_group(backend='nccl')
+        
+        @contextlib.contextmanager
+        def maybe_fp8_ctx():
+            with te.fp8_autocast(enabled=use_fp8, fp8_recipe=fp8_recipe, fp8_group=all_gpus):
+                yield
+    else:
+        maybe_fp8_ctx = nullcontext
 
     # Training loop
     loop_iter = configure_train_loop(data_loader, profile, output_path, cfg_m, bsz, use_fp8, rank)
@@ -107,7 +110,7 @@ def train_ddp(
         input_BT, label_BT = map(lambda t: t.pin_memory().to(rank), data_batch)
 
         with torch.amp.autocast('cuda', torch.bfloat16):
-            with te.fp8_autocast(enabled=use_fp8, fp8_recipe=fp8_recipe, fp8_group=all_gpus):
+            with maybe_fp8_ctx():
                 weight_cache = use_fp8 and (step_idx % grad_acc_steps == 0)
                 logits_BTV = model(input_BT, is_first_microbatch=weight_cache)
                 loss = F.cross_entropy(logits_BTV.flatten(0, 1), label_BT.flatten())
--- train_fsdp.py
+++ train_fsdp.py
@@ -1,10 +1,10 @@
+import contextlib
 from dataclasses import asdict
 from pathlib import Path
 
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from torch.utils.data import DataLoader
 
 from utils import *
 
@@ -12,7 +12,6 @@ from utils import *
 import os
 import torch.multiprocessing as mp
 from torch.distributed import init_process_group, destroy_process_group
-from torch.utils.data.distributed import DistributedSampler
 
 # FSDP
 from functools import partial
@@ -41,7 +40,6 @@ from transformer_engine.pytorch.distributed import prepare_te_modules_for_fsdp
 def train(
     cfg_path: str,
     bsz: int = 8,
-    n_workers: int = 8,
     n_steps: int = 128*8,
     grad_acc_steps: int = 8,
     reduce_freq: int = 32,
@@ -55,7 +53,6 @@ def train(
     '''
     :param       cfg_path: Model configuration file path
     :param            bsz: Batch size
-    :param      n_workers: Number of CPUs for data loading
     :param        n_steps: Number of training steps
     :param grad_acc_steps: Number of gradient accumulation steps
     :param    reduce_freq: Number of steps FSDP performs an all gather
@@ -70,7 +67,7 @@ def train(
     world_size = torch.cuda.device_count()
     train_args = (
         world_size,
-        cfg_path, bsz, n_workers, n_steps, grad_acc_steps, reduce_freq,
+        cfg_path, bsz, n_steps, grad_acc_steps, reduce_freq,
         sac_freq, use_fp8, pt_compile, compile_mode, profile, output_dir
     )
     assert not (use_fp8 and (sac_freq != '1/1')), 'Selective AC currently doesn\'t work with Transformer Engine.'
@@ -84,7 +81,7 @@ def train(
 
 def train_fsdp(
     rank, world_size,
-    cfg_path, bsz, n_workers, n_steps, grad_acc_steps, reduce_freq,
+    cfg_path, bsz, n_steps, grad_acc_steps, reduce_freq,
     sac_freq, use_fp8, pt_compile, compile_mode, profile, output_dir
 ):
     # Construct process group
@@ -98,12 +95,7 @@ def train_fsdp(
     dprint(rank, f'Loaded {model_cls} model.', end=' ')
     cfg_m.estimate_flops_per_token(model, bsz, rank)  # Need to do before wrapping in FSDP
 
-    dataset = DummyDataset(cfg_m.vocab_size, cfg_m.max_seq_len, bsz*n_steps)
-    data_loader = DataLoader(
-        dataset, batch_size=bsz,
-        num_workers=n_workers, pin_memory=True, shuffle=False,
-        sampler=DistributedSampler(dataset, rank=rank, num_replicas=world_size, shuffle=True)
-    )
+    data_loader = create_distributed_data_loader(rank, world_size, bsz, n_steps, cfg_m)
     optimizer = torch.optim.AdamW(model.parameters(), fused=True)
     scheduler = torch.optim.lr_scheduler.LambdaLR(optimizer, lambda t: 1.0)
 
@@ -111,7 +103,6 @@ def train_fsdp(
     output_path = f'{output_dir}/{Path(cfg_path).stem}_fsdp_trace.json'
 
     # FSDP
-    all_gpus = dist.new_group(backend='nccl')
     model = FSDP(
         model,
         device_id=rank,
@@ -148,20 +139,29 @@ def train_fsdp(
         dprint(rank, f'Compiling in {compile_mode} mode')
         model = torch.compile(model, mode=compile_mode)
 
-    # FP8
-    fp8_format = Format.HYBRID  # E4M3 during forward pass, E5M2 during backward pass
-    fp8_recipe = DelayedScaling(fp8_format=fp8_format, amax_history_len=16, amax_compute_algo='max')
-
     # Training loop
     loop_iter = configure_train_loop(data_loader, profile, output_path, cfg_m, bsz, use_fp8, rank)
     ddp_loss = torch.zeros(2, device=rank)
     model.train()
+    
+    if use_fp8:
+        # FP8
+        fp8_format = Format.HYBRID  # E4M3 during forward pass, E5M2 during backward pass
+        fp8_recipe = DelayedScaling(fp8_format=fp8_format, amax_history_len=16, amax_compute_algo='max')
+        all_gpus = dist.new_group(backend='nccl')
+        
+        @contextlib.contextmanager
+        def maybe_fp8_ctx():
+            with te.fp8_autocast(enabled=use_fp8, fp8_recipe=fp8_recipe, fp8_group=all_gpus):
+                yield
+    else:
+        maybe_fp8_ctx = nullcontext
 
     for step_idx, data_batch in loop_iter:
         input_BT, label_BT = map(lambda t: t.pin_memory().to(rank), data_batch)
 
         with torch.amp.autocast('cuda', torch.bfloat16):
-            with te.fp8_autocast(enabled=use_fp8, fp8_recipe=fp8_recipe, fp8_group=all_gpus):
+            with maybe_fp8_ctx():
                 weight_cache = use_fp8 and (step_idx % grad_acc_steps == 0)
                 logits_BTV = model(input_BT, is_first_microbatch=weight_cache)
                 loss = F.cross_entropy(logits_BTV.flatten(0, 1), label_BT.flatten())
