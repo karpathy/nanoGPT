c7353af9503be91e756ae95fce32d9e1727b6889
Santiago Akle <sakle@nvidia.com>
2024-10-14 20:47:33 -0700

    Enable initial eval

--- train.py
+++ train.py
@@ -262,7 +262,7 @@ while True:
         param_group['lr'] = lr
 
     # evaluate the loss on train/val sets and write checkpoints
-    if iter_num % eval_interval == 0 and iter_num > 0 and master_process:
+    if iter_num % eval_interval == 0 and master_process:
         losses = estimate_loss()
         print(f"step {iter_num}: train loss {losses['train']:.4f}, val loss {losses['val']:.4f}")
         if wandb_log:
