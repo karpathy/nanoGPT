a5bc6317ec57e65379ac9afe174e3620802818b3
oren <oren.leung@gmail.com>
2024-10-21 03:57:44 +0000

    Tested SWA and added FP8 model.

--- mistral.py
+++ mistral.py
@@ -24,11 +24,14 @@ class MistralConfig:
 
     def estimate_flops_per_token(self, model, bsz, rank=0):
         head_dim = self.d_embd // self.n_heads
-        N = sum(p.numel() for p in model.parameters())         # get param count
-        density = self.window_size / self.max_seq_len  # sliding window attention mask
+        N = sum(p.numel() for p in model.parameters())  # get param count
+
+        W, T = self.window_size, self.max_seq_len
+        density = (W * (W - 1) / 2 + (T - W + 1) * W) / (T * T)
+
         self.flops_per_token = 6 * N + 12 * self.n_layers * self.n_heads * head_dim * self.max_seq_len * density
         if rank == 0:
-            print(f"Number of parameters: {N/1e9:.2f}B")    # print number of billion parameters
+            print(f"Number of parameters: {N/1e9:.2f}B")  # print number of billion parameters
 
 
 class GroupedQueryAttention(nn.Module):
@@ -149,11 +152,58 @@ def precompute_freq_cis(dim, rope_base, max_seq_len, **kwargs):
 
 
 def create_sliding_window_attention_mask(window_size, max_seq_len):
-    rows = torch.arange(max_seq_len).unsqueeze(1)  # [T, 1]
-    cols = torch.arange(max_seq_len).unsqueeze(0)  # [1, T]
-    l1_dist = rows - cols
-    swa_mask = (l1_dist >= 0) & (l1_dist < window_size)
-    return swa_mask
+    q_idx = torch.arange(max_seq_len).unsqueeze(1)   # [T, 1]
+    kv_idx = torch.arange(max_seq_len).unsqueeze(0)  # [1, T]
+    causal_mask = (q_idx >= kv_idx)
+    window_mask = (q_idx - kv_idx < window_size)
+    return causal_mask & window_mask
+
+
+class Fp8Mistral(nn.Module):
+    def __init__(self, vocab_size, d_embd, n_layers, n_heads, window_size, max_seq_len, **kwargs):
+        super().__init__()
+        self.tok_embd = nn.Embedding(vocab_size, d_embd)
+        self.tsfmr_blks = nn.ModuleList(
+            Fp8MistralBlock(d_embd, n_heads=n_heads, window_size=window_size, **kwargs) for _ in range(n_layers)
+        )
+        self.norm_lm_head = te.LayerNormLinear(
+            d_embd, vocab_size, bias=False,
+            normalization='RMSNorm', eps=kwargs['norm_eps']
+        )
+        freq_cis_TE = te.attention.RotaryPositionEmbedding(d_embd//n_heads)(max_seq_len=max_seq_len)
+        self.register_buffer('freq_cis_TE', freq_cis_TE.to(torch.bfloat16))
+        self.register_buffer('swa_mask', create_sliding_window_attention_mask(window_size, max_seq_len))
+
+    def forward(self, idx_BT, is_first_microbatch):
+        x_BTE = self.tok_embd(idx_BT)
+        for tsfmr_blk in self.tsfmr_blks:
+            x_BTE = tsfmr_blk(
+                x_BTE,
+                rotary_pos_emb=self.freq_cis_TE,
+                attention_mask=self.swa_mask,
+                is_first_microbatch=is_first_microbatch
+            )
+        logits_BTV = self.norm_lm_head(x_BTE)
+        return logits_BTV
+
+
+class Fp8MistralBlock(te.TransformerLayer):
+    def __init__(self, d_embd, d_hid, n_heads, n_kv_heads, window_size, norm_eps, **kwargs):
+        super().__init__(
+            hidden_size=d_embd,
+            num_attention_heads=n_heads,
+            num_gqa_groups=n_heads//n_kv_heads,
+            fuse_qkv_params=True,
+            attn_input_format='bshd',
+            self_attn_mask_type='arbitrary',  # attn mask is ignored if not set
+            attention_dropout=0.0,
+            normalization='RMSNorm',
+            layernorm_epsilon=norm_eps,
+            ffn_hidden_size=d_hid,
+            bias=False,
+            activation='swiglu',
+            hidden_dropout=0.0
+        )
 
 
 if __name__ == '__main__':
@@ -167,8 +217,8 @@ if __name__ == '__main__':
         d_embd=4096,
         d_hid=14336,
         vocab_size=32000,
-        max_seq_len=32768,
-        window_size=4096,
+        max_seq_len=8192,
+        window_size=1024,
         rope_base=1e4,
         norm_eps=1e-5
     )
