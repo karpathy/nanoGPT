383ad8222f1e7cabc18f4b3f68d4ca8b08c63e11
Chris Kerwell Gresla <ckgresla@gmail.com>
2024-10-26 13:31:57 -0700

    fix: useless stuff to the shadow realm

--- config/eval_gpt2_xl.py
+++
@@ -1,8 +0,0 @@
-# evaluate the base gpt2
-# n_layer=48, n_head=25, n_embd=1600
-# 1558M parameters
-batch_size = 8
-eval_iters = 500 # use more iterations to get good estimate
-eval_only = True
-wandb_log = False
-init_from = 'gpt2-xl'
--- data/numbers/readme.md
+++
@@ -1,9 +0,0 @@
-
-# numbers in the range [0, 9]
-
-let us teach a transformer to count
-
-After running `prepare.py`:
-
-- train.bin has 1,003,854 tokens
-- val.bin has 111,540 tokens
