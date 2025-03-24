6bd13f3f15e69a02f613a36e3b0f6dc5f2ad7cb9
biancaganescu <bianca.ganescu1@gmail.com>
2025-03-24 19:17:14 +0000

    added check data file

--- check_data.py
+++ check_data.py
@@ -1,6 +1,6 @@
 import torch
 
-torch.set_printoptions(precision=10, threshold=float('inf'))
+torch.set_printoptions(threshold=float('inf'))
 data = torch.load("./data/card_set/train.pt")
 
 print(data[0:2])
@@ -10,11 +10,14 @@ for item in data:
     # Check if the item has indexable structure
     if hasattr(item, "__getitem__"):
         try:
-            if item[-2] == 4:
-                count += 1
+            sequence_len = len(item) // 18
+
+            for i in range(sequence_len):
+                if item[i * 18 + 16] == 4:
+                    count += 1
         except (IndexError, TypeError):
             print("Item doesn't have a penultimate element or isn't indexable as expected")
             break
 
 print(f"Count of items with value 4 at penultimate position: {count}")
-print(f"Percentage: {(count / len(data)) * 100:.2f}%")
+print(f"Percentage: {(count / (len(data) * sequence_len)) * 100:.2f}%")
