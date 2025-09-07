51ea3d99c4e24d872ceb258f9480e06e91fc825f
RishavAich511 <aichrishav2003@gmail.com>
2025-09-07 17:34:54 +0530

    removed processed dir

--- data/kant/prepare.py
+++ data/kant/prepare.py
@@ -35,7 +35,7 @@ print(f"train has {len(train_ids):,} tokens")
 print(f"val has {len(val_ids):,} tokens")
 
 # Export to bin files
-os.makedirs(os.path.join(os.path.dirname(__file__), 'processed'), exist_ok=True)
+os.makedirs(os.path.join(os.path.dirname(__file__)), exist_ok=True)
 train_ids = np.array(train_ids, dtype=np.uint16)
 val_ids   = np.array(val_ids, dtype=np.uint16)
 train_ids.tofile(os.path.join(os.path.dirname(__file__), 'processed/train.bin'))
