0edcae52272181b22c78d8e235525fee85eb44b2
RishavAich511 <aichrishav2003@gmail.com>
2025-09-08 11:24:34 +0530

    removed unnecessary downloads

--- data/kant/prepare.py
+++ data/kant/prepare.py
@@ -8,12 +8,6 @@ import pickle
 # Path to dataset
 input_file_path = os.path.join(os.path.dirname(__file__), 'data1.txt')
 
-# Download if not exists (replace URL if you want other text)
-if not os.path.exists(input_file_path):
-    data_url = 'https://raw.githubusercontent.com/karpathy/char-rnn/master/data/tinyshakespeare/input.txt'
-    with open(input_file_path, 'w', encoding='utf-8') as f:
-        f.write(requests.get(data_url).text)
-
 # Read dataset
 with open(input_file_path, 'r', encoding='utf-8') as f:
     data = f.read()
