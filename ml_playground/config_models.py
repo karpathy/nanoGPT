7ab5c4236852876c04c90df90de183181cdcb1cf
Thomas Vogt <mehrmorgen@me.com>
2025-09-09 22:47:25 +0200

    refactor: remove deprecated config_models module in favor of config module

--- ml_playground/config_models.py
+++
@@ -1,6 +0,0 @@
-from __future__ import annotations
-
-raise ImportError(
-    "ml_playground.config_models has been removed. Import from 'ml_playground.config' instead "
-    "(e.g., 'from ml_playground.config import RuntimeConfig')."
-)
