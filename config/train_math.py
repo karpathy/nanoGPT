45a98a43b848d7134d6d9043625eb0d8b83c82c2
Wesley Jochman <wjochman@gmail.com>
2025-10-13 19:36:35 -0500

    Updates

--- config/train_math.py
+++ config/train_math.py
@@ -1,7 +1,7 @@
 # train a miniature character-level shakespeare model
 # good for debugging and playing on macbooks and such
 
-out_dir = "out-shakespeare-char"
+out_dir = "out-math"
 eval_interval = 250  # keep frequent because we'll overfit
 eval_iters = 200
 log_interval = 10  # don't print too too often
@@ -9,14 +9,16 @@ log_interval = 10  # don't print too too often
 # we expect to overfit on this small dataset, so only save when val improves
 always_save_checkpoint = False
 
+init_from = "resume"
+
 wandb_log = False  # override via command line if you like
-wandb_project = "shakespeare-char"
+wandb_project = "math"
 wandb_run_name = "mini-gpt"
 
-dataset = "shakespeare_char"
+dataset = "math"
 gradient_accumulation_steps = 1
 batch_size = 64
-block_size = 256  # context of up to 256 previous characters
+block_size = 256
 
 # baby GPT model :)
 n_layer = 6
