a050823acd84968ffb4e58613fa51acbe029d950
jochmanw <jochmanw@ad.msoe.edu@dh-node1.hpc.msoe.edu>
2025-10-13 20:55:41 -0400

    push some data

--- config/train_math.py
+++ config/train_math.py
@@ -9,8 +9,6 @@ log_interval = 10  # don't print too too often
 # we expect to overfit on this small dataset, so only save when val improves
 always_save_checkpoint = False
 
-init_from = "resume"
-
 wandb_log = False  # override via command line if you like
 wandb_project = "math"
 wandb_run_name = "mini-gpt"
