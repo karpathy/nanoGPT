e2221318e607eb5ee0669d6b7641af25a2111545
Andreea Lupu <0607andreea.ro@gmail.com>
2026-03-24 15:08:05 +0200

    finetuning for owt T4

--- train.py
+++ train.py
@@ -40,8 +40,8 @@ eval_only = False # if True, script exits right after the first eval
 always_save_checkpoint = True # if True, always save a checkpoint after each eval
 init_from = 'scratch' # 'scratch' or 'resume' or 'gpt2*'
 # wandb logging
-wandb_log = False # disabled by default
-wandb_project = 'owt'
+wandb_log = True # disabled by default
+wandb_project = 'nanogpt'
 wandb_run_name = 'gpt2' # 'run' + str(time.time())
 # data
 dataset = 'openwebtext'
