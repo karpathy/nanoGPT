bbb3af16dfb7c35d03aee15280a37157c97d6817
Andreea Lupu <0607andreea.ro@gmail.com>
2026-04-18 22:29:06 +0300

    sweep run: enriched sweep run

--- h2/h2_eval_sweep.py
+++ h2/h2_eval_sweep.py
@@ -2,7 +2,7 @@ import torch, math, json, os
 import wandb
 
 # L_VALUES  = [0, 64, 100, 256, 512, 1024, 2048]
-L_VALUES  = [100]
+L_VALUES  = [0,64,256,512]
 M_VALUES = [1, 5, 10, 20]
 TASK      = "wikitext2"
 DEVICE    = "cuda"
