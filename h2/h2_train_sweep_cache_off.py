be24955210ccb1b8422d5729e54431913e3e6ff4
Andreea Lupu <0607andreea.ro@gmail.com>
2026-05-22 09:34:44 +0300

    Sweep for L = 256, 512, 760

--- h2/h2_train_sweep_cache_off.py
+++ h2/h2_train_sweep_cache_off.py
@@ -3,11 +3,10 @@ import subprocess, os, time, torch, json, math
 # RUN_ORDER = [100, 0, 64, 256, 512, 1024, 2048]
 # RUN_ORDER = [0, 16, 32, 80, 100, 256, 512, 760]
 # M_VALUES = [1, 5, 10, 20]
-# RUN_ORDER = [64, 80, 100, 256, 512, 760]
-# M_VALUES = [1, 5, 10, 20]
+RUN_ORDER = [ 256, 512, 760]
+M_VALUES = [1, 5, 10, 20]
 
-RUN_ORDER = [100]
-M_VALUES = [5, 10, 20]
+
 TASK      = "wikitext2"
 
 results         = []
