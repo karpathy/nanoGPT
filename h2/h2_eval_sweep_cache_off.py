1d32e6e3002b78869941bc0cfae0b76ab2727744
Andreea Lupu <0607andreea.ro@gmail.com>
2026-05-26 14:26:39 +0300

    Eval sweeps

--- h2/h2_eval_sweep_cache_off.py
+++ h2/h2_eval_sweep_cache_off.py
@@ -2,7 +2,7 @@ import torch, math, json, os
 import wandb
 
 # L_VALUES  = [0, 64, 100, 256, 512, 1024, 2048]
-L_VALUES  = [0, 64, 100, 256, 512]
+L_VALUES  = [0, 16, 32, 64, 80, 100, 256, 512, 760]
 M_VALUES = [1, 5, 10, 20]
 TASK      = "wikitext2"
 DEVICE    = "cuda"
