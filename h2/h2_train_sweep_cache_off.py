537b8ca04a3282572129b9bd24697fabfabd8c37
Andreea Lupu <0607andreea.ro@gmail.com>
2026-05-27 08:22:37 +0300

    cache off sweep

--- h2/h2_train_sweep_cache_off.py
+++ h2/h2_train_sweep_cache_off.py
@@ -1,7 +1,7 @@
 import subprocess, os, time, torch, json, math
 
 # RUN_ORDER = [100, 0, 64, 256, 512, 1024, 2048]
-RUN_ORDER = [0, 16, 32, 64, 80, 100, 256, 512, 760]
+RUN_ORDER = [0, 16, 32, 80, 100, 256, 512, 760]
 M_VALUES = [1, 5, 10, 20]
 
 
