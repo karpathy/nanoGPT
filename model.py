3774783d74bec96b015a7c7878fc95b03f6fd821
PNAGESW2 <pnagesw2@ford.com>
2026-07-31 07:19:59 +0530

    ft:Response streaming added

--- model.py
+++ model.py
@@ -328,3 +328,18 @@ class GPT(nn.Module):
             idx = torch.cat((idx, idx_next), dim=1)
 
         return idx
+
+    @torch.no_grad()
+    def generate_stream(self, idx, max_new_tokens, decode_fn, temperature=0.8, top_k=40):
+        for _ in range(max_new_tokens):
+            idx_cond = idx[:, -self.config.block_size:]
+            logits, _ = self(idx_cond)
+            logits = logits[:, -1, :] / temperature
+            if top_k is not None:
+                v, _ = torch.topk(logits, min(top_k, logits.size(-1)))
+                logits[logits < v[:, [-1]]] = float('-inf')
+            probs = F.softmax(logits, dim=-1)
+            idx_next = torch.multinomial(probs, num_samples=1)
+            idx = torch.cat((idx, idx_next), dim=1)
+            print(decode_fn([idx_next.item()]), end='', flush=True)
+        return idx
